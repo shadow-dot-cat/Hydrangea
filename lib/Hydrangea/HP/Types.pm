3c1689a2ca32e6a8c0a6531a3c9fd72e26ddf646
Matt S Trout <mst@shadowcat.co.uk>
2019-08-02 00:39:52 +0000

    cleanup types

--- lib/Hydrangea/HP/Types.pm
+++ lib/Hydrangea/HP/Types.pm
@@ -1,64 +1,71 @@
 package Hydrange::HP::Types;
 
 use Hydrangea::Package;
-use Type::Library -base;
+use Type::Library -base, -declare => 'JBool';
 use Types::Standard -types;
 use Type::Utils -all;
 
-declare JBool => InstanceOf['JSON::PP::Boolean'] | ScalarRef[Bool];
+BEGIN {
+  no warnings 'redefine';
+  my $tuple = __PACKAGE__->can('Tuple');
+  *Tuple = sub :prototype(;$) {
+    $tuple->([ map +(ref($_) ? $_ : Enum[$_]), @$_ ]);
+  }
+}
 
-declare Client_Protocol_Offer => Tuple[
+declare JBool, as (InstanceOf['JSON::PP::Boolean'] | ScalarRef[Bool]);
+
+declare Client_Protocol_Offer => as Tuple[
   protocol_offer => hydrangea => Num
 ];
 
-declare Trunk_Protocol_Accept => Tuple[
+declare Trunk_Protocol_Accept => as Tuple[
   protocol_accept => hydrangea => Num
 ];
 
-declare Client_Ident_Assert => Tuple[
+declare Client_Ident_Assert => as Tuple[
   ident_assert => Str, Str
 ];
 
-declare Trunk_Ident_Confirm => Tuple[
+declare Trunk_Ident_Confirm => as Tuple[
   ident_confirm => Str
 ];
 
-declare Client_Message_From => Tuple[
+declare Client_Message_From => as Tuple[
   message_from
     => Dict[ venue => Optional[Str], nick => Str, user => Optional[Str] ]
     => Dict[ raw_text => Str, text => Str, is_to_me => JBool ]
 ];
 
-declare Trunk_Message_To => Tuple[
+declare Trunk_Message_To => as Tuple[
   message_to
     => Dict[ venue => Optional[Str], nick => Str ]
     => Str
+];
 
-;
+declare Client_Command_Register => as Tuple[ command_register => Str ];
 
-declare Client_Command_Register => Tuple[ command_register => Str ];
-
-declare Trunk_Command_Start => Tuple[
+declare Trunk_Command_Start => as Tuple[
   command_start => Str, Str, Dict[ raw => Str, args => ArrayRef ]
 ];
 
-declare Trunk_Command_Send => Tuple[
+declare Trunk_Command_Send => as Tuple[
   command_send => Str, Str
 ];
 
-declare Trunk_Command_Cancel => Tuple[
+declare Trunk_Command_Cancel => as Tuple[
   command_cancel => Str
 ];
 
-declare Client_Command_Sent => Tuple[
+declare Client_Command_Sent => as Tuple[
   command_sent => Str, Str
 ];
 
-declare Client_Command_Done => Tuple[
+declare Client_Command_Done => as Tuple[
   command_done => Str, Optional[Str]
 ];
 
-declare Client_Command_Failed => Tuple[
+declare Client_Command_Failed => as Tuple[
   command_failed => Str, Str
 ];
 
